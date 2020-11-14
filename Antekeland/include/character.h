0f5eeee4eb7bb0226fc82db0fb1b2946a3da1d43
Ricardo <beltranmu@esat-alumni.com>
2020-11-14 18:12:33 +0100

    chartacter added

---
+++ Antekeland/include/character.h
@@ -0,0 +1,55 @@
+/*
+* Antekeland 2077
+* Author: Javier Benito Abolafio & Ricardo Beltrán Muriel
+* Mail: benitoab@esat-alumni.com & beltranmu@esat-alumni.com
+* University Development @ESAT
+*/
+
+#ifndef __CHARACTER_H__
+#define __CHARACTER_H__ 1 
+
+#include "entity.h"
+
+enum kEnumProfession{
+  kEnumProfession_Warrior = 0,
+  kEnumProfession_Hunter ,
+  kEnumProfession_Rogue ,
+  kEnumProfession_Mage ,
+  kEnumProfession_Warlock ,
+  kEnumProfession_Shepherd  ,
+  kEnumProfession_Monster ,
+};
+
+class Character : public Entity{
+
+  public :
+  Character();
+  ~Character();
+  
+  void init();
+  
+  void levelUp();
+  void updatePosition(); 
+  
+  uint8_t Character::cell();
+  
+  void draw(SDL_Renderer* ren);
+  
+  protected:
+  
+  RBM::Vec2 velocity;
+  
+  int32_t hp_;
+  int32_t attack_;
+  int32_t defense_;
+  int32_t movement_;
+  int32_t profession_;
+  int32_t xp_;
+  int32_t level_;
+  
+  
+
+};
+
+
+#endif
